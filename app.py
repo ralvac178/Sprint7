ef6c8e3fb07a602a100dd9549de9da04a6383a13
Roy Alva <52391112+ralvac178@users.noreply.github.com>
2025-10-24 16:51:20 -0500

    Added a button

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ import numpy as np
 
 st.title("Visualización de Datos de Vehículos")
 
-car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-histogram = px.histogram(car_data, x="odometer")  # crear un histograma
-# crear un gráfico de dispersión
-dispersion = px.scatter(car_data, x="odometer", y="price")
-# Mostrar en Streamlit
-st.title("Histograma de Odómetro")
-st.plotly_chart(histogram)
-st.title("Dispersion de Precio vs Odómetro")
-st.plotly_chart(dispersion)
+if st.button("Mostrar los datos"):
+    car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+    histogram = px.histogram(car_data, x="odometer")  # crear un histograma
+    # crear un gráfico de dispersión
+    dispersion = px.scatter(car_data, x="odometer", y="price")
+    # Mostrar en Streamlit
+    st.title("Histograma de Odómetro")
+    st.plotly_chart(histogram)
+    st.title("Dispersion de Precio vs Odómetro")
+    st.plotly_chart(dispersion)
+else:
+    st.write("Presiona el botón para mostrar los datos de vehículos.")
