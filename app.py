35f4c172a1c5e8a8ed2adae8c5f2f27b1d9d38ec
Roy Alva <52391112+ralvac178@users.noreply.github.com>
2025-10-24 16:46:26 -0500

    Modified Graphs

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
-st.title("Histograma con Plotly")
+st.title("Visualización de Datos de Vehículos")
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-fig = px.histogram(car_data, x="odometer")  # crear un histograma
-
+histogram = px.histogram(car_data, x="odometer")  # crear un histograma
+# crear un gráfico de dispersión
+dispersion = px.scatter(car_data, x="odometer", y="price")
 # Mostrar en Streamlit
-st.plotly_chart(fig)
+st.title("Histograma de Odómetro")
+st.plotly_chart(histogram)
+st.title("Dispersion de Precio vs Odómetro")
+st.plotly_chart(dispersion)
