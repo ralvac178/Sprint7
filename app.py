58d88c62032a10da80d4c34a4ef1f3d32ddf9953
Roy Alva <52391112+ralvac178@users.noreply.github.com>
2025-10-24 08:18:53 -0500

    Added app.py file

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+start_button = st.button('Ejecutar')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
